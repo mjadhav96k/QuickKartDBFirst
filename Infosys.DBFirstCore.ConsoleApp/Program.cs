ad68ff5c0b9fa07650c4365bfea76b69bf1a37ed
mayur <mjm090972@gmail.com>
2025-02-05 16:09:22 +0530

    CRUD Opertions using DBFirst Approach along with demo and excercises

--- Infosys.DBFirstCore.ConsoleApp/Program.cs
+++ Infosys.DBFirstCore.ConsoleApp/Program.cs
@@ -1,10 +1,217 @@
-﻿namespace Infosys.DBFirstCore.ConsoleApp
+﻿using System;
+using System.Net;
+using System.Reflection;
+using Infosys.DBFirstCore.DataAccessLayer;
+using Infosys.DBFirstCore.DataAccessLayer.Models;
+namespace Infosys.DBFirstCore.ConsoleApp
 {
-    internal class Program
+    public class Program
     {
+        static QuickKartDbContext context;
+        static QuickKartRepository repository;
+
+        static Program()
+        {
+            context = new QuickKartDbContext();
+            repository = new QuickKartRepository(context);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region READ
+            //var categories = repository.GetAllCategories();
+            //Console.WriteLine("----------------------------------");
+            //Console.WriteLine("CategoryId\tCategoryName");
+            //Console.WriteLine("----------------------------------");
+            //foreach (var category in categories)
+            //{
+            //    Console.WriteLine("{0}\t\t{1}", category.CategoryId, category.CategoryName);
+            //}
+
+            ////////////////////////////////////////////////////////////////////////////////////////
+            //byte categoryId = 1;
+            //List<Product> lstProducts = repository.GetProductsOnCategoryId(categoryId);
+
+            //if (lstProducts.Count == 0)
+            //{
+            //    Console.WriteLine("No products available under the category = " + categoryId);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+            //    Console.WriteLine("---------------------------------------------------------------------------------------");
+            //    foreach (var product in lstProducts)
+            //    {
+            //        Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+            //    }
+            //}
+
+            ///////////////////////////////////////////////////////////////////////////////////////////
+            //byte categoryId = 1;
+            //Product product = repository.FilterProducts(categoryId);
+            //if (product == null)
+            //{
+            //    Console.WriteLine("No product details available");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+            //    Console.WriteLine("---------------------------------------------------------------------------------------");
+            //    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+            //}
+            //Console.WriteLine();
+
+            //string pattern = "BMW%";
+            //List<Product> lstProducts = repository.FilterProductsUsingLike(pattern);
+            //if (lstProducts.Count == 0)
+            //{
+            //    Console.WriteLine("No products available with the = " + pattern);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+            //    Console.WriteLine("---------------------------------------------------------------------------------------");
+            //    foreach (var product in lstProducts)
+            //    {
+            //        Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+            //    }
+            //}
+            //Console.WriteLine(); 
+
+            //var userlist = repository.GetAllUsers();
+            //Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+            //Console.WriteLine("{0,-30}{1,-20}{2,-10 }{3,-10}{4}", "EmailID", "UserPassword", "RoleId", "Gender", "DateOfBirth");
+            //Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+            //foreach (var users in userlist)
+            //{
+            //    Console.WriteLine("{0,-30}{1,-20}{2,-10}{3,-10}{4}", users.EmailId, users.UserPassword, users.RoleId, users.Gender, users.DateOfBirth);
+            //}
+            #endregion
+
+            #region CREATE
+            ////////////////////////////////////////////////////////////////////////////
+            //bool result = repository.AddCategory("Books");
+            //if (result)
+            //{
+            //    Console.WriteLine("New category added successfully");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Something went wrong. Try again!");
+            //}
+
+            //////////////////////////////////////////////////////////////////////////////
+            //Product productOne = new Product();
+            //productOne.ProductId = "P158";
+            //productOne.ProductName = "The Ship of Secrets - Geronimo Stilton";
+            //productOne.CategoryId = 8;
+            //productOne.Price = 450;
+            //productOne.QuantityAvailable = 10;
+            //Product productTwo = new Product();
+            //productTwo.ProductId = "P159";
+            //productTwo.ProductName = "101 Nursery Rhymes";
+            //productTwo.CategoryId = 8;
+            //productTwo.Price = 700;
+            //productTwo.QuantityAvailable = 10;
+            //bool result = repository.AddProductsUsingAddRange(productOne, productTwo);
+            //if (result)
+            //{
+            //    Console.WriteLine("Product details added successfully!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Some error occurred. Try again!!");
+            //}
+
+            ////////////////////////////////////////////////////////////////////////////
+
+            //bool result = repository.RegisterUser("GEAHN@123", "F", "Meghan@gmail.com", Convert.ToDateTime("02-dec-1978"), "New avenue, Robster");
+            //if (result)
+            //{
+            //    Console.WriteLine("New user details added successfully");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Add a new user failed");
+            //} 
+            #endregion
+
+            #region UPDATE
+            //Connected Scenario
+            //bool result = repository.UpdateCategory(8, "Stationery");
+            //if (result)
+            //{
+            //    Console.WriteLine("Category details updated successfully");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Something went wrong. Try again!");
+            //}
+
+            //Disconnected Scenario
+            //int status = repository.UpdateProduct("P159", 1000);
+            //if (status == 1)
+            //{
+            //    Console.WriteLine("Product price updated successfully!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Some error occurred. Try again!!");
+            //}
+
+            //Update Multiple Records
+            //int status = repository.UpdateProductsUsingUpdateRange(8, 10);
+            //if (status == 1)
+            //{
+            //    Console.WriteLine("Products updated successfully!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Some error occurred. Try again!!");
+            //}
+
+            //Excercise
+            //bool result = repository.UpdateUserPassword("Meghan@gmail.com", "GEAHNM@1234");
+            //if (result)
+            //{
+            //    Console.WriteLine("New user password updated successfully");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Something went wrong. Try again!");
+            //} 
+            #endregion
+
+            #region DELETE
+            //bool status = repository.DeleteProduct("P159");
+            //if (status)
+            //{
+            //    Console.WriteLine("Product details deleted successfully!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Some error occurred. Try again!!");
+            //}
+
+            //bool status = repository.DeleteProductsUsingRemoveRange("BMW");
+            //if (status)
+            //{
+            //    Console.WriteLine("Products deleted successfully");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Some error occurred.Try again!!");
+            //}
+
+            //bool status = repository.DeleteUserDetails("mjm090972@gmail.com");
+            //if (status)
+            //{
+            //    Console.WriteLine("User details deleted successfully!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Some error occurred. Try again!!");
+            //} 
+            #endregion
         }
     }
 }
---
+++ Infosys.DBFirstCore.DataAccessLayer/QuickKartRepository.cs
@@ -0,0 +1,321 @@
+﻿using Infosys.DBFirstCore.DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infosys.DBFirstCore.DataAccessLayer
+{
+    public class QuickKartRepository
+    {
+        private QuickKartDbContext context;
+
+        public QuickKartRepository(QuickKartDbContext context)
+        {
+            this.context = context;
+        }
+
+        #region READ
+        public List<Category> GetAllCategories()
+        {
+            //var categoriesList = (from category in context.Categories
+            //                      orderby category.CategoryId
+            //                      select category).ToList(); // Query syntax
+            var categoriesList = context.Categories.OrderBy(c => c.CategoryId).ToList(); // Method syntax
+            return categoriesList;
+        }
+
+        public List<Product> GetProductsOnCategoryId(byte categoryId)
+        {
+            List<Product> lstProducts = new List<Product>();
+            try
+            {
+                lstProducts = context.Products.Where(p => p.CategoryId == categoryId).ToList();
+            }
+            catch (Exception)
+            {
+                lstProducts = null;
+            }
+            return lstProducts;
+        }
+
+        public Product FilterProducts(byte categoryId)
+        {
+            Product product = new Product();
+            try
+            {
+                //product = context.Products.Where(p => p.CategoryId == categoryId).FirstOrDefault();
+
+                product = context.Products.Where(p => p.CategoryId == categoryId)
+                                     .OrderByDescending(p => p.Price)
+                                     .LastOrDefault();
+            }
+            catch (Exception)
+            {
+                product = null;
+                throw;
+            }
+            return product;
+        }
+
+        public List<Product> FilterProductsUsingLike(string pattern)
+        {
+            List<Product> lstProduct = new List<Product>();
+            try
+            {
+                lstProduct = context.Products.Where(p => EF.Functions.Like(p.ProductName, pattern)).ToList();
+            }
+            catch (Exception)
+            {
+                lstProduct = null;
+            }
+            return lstProduct;
+        }
+
+        //Excercise
+        public List<User> GetAllUsers()
+        {
+            var lstUsers = context.Users.OrderBy(u => u.DateOfBirth).ToList();
+            return lstUsers;
+        }
+        #endregion
+
+        #region CREATE
+        public bool AddCategory(string categoryName)
+        {
+            bool status = false;
+            Category category = new Category();
+            category.CategoryName = categoryName;
+            try
+            {
+                //context.Categories.Add(category);
+                context.Add<Category>(category);
+                int count = context.SaveChanges();
+                Console.WriteLine("Number of records affected: " + count);
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+        public bool AddProductsUsingAddRange(params Product[] products)
+        {
+            bool status = false;
+            try
+            {
+                context.Products.AddRange(products);
+                int count = context.SaveChanges();
+                Console.WriteLine("Number of records affected: " + count);
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public bool RegisterUser(string userPassword, string gender, string emailId, DateTime dateOfBirth, string address)
+        {
+            bool status = false;
+            try
+            {
+                string temp = dateOfBirth.ToString("yyyy-MM-dd");
+                DateOnly dateOnly = DateOnly.Parse(temp);
+
+                User user = new User();
+                user.UserPassword = userPassword;
+                user.Gender = gender;
+                user.EmailId = emailId;
+                user.DateOfBirth = dateOnly;
+                user.Address = address;
+                context.Users.Add(user);
+                int count = context.SaveChanges();
+                Console.WriteLine("Number of records affected: " + count);
+            }
+            catch (Exception)
+            {
+                status = false;
+                throw;
+            }
+
+            return status;
+        }
+        #endregion
+
+        #region UPDATE
+        public bool UpdateCategory(byte categoryId, string newCategoryName)
+        {
+            //Category category = context.Categories.Where(c => c.CategoryId == categoryId).FirstOrDefault();
+            bool status = false;
+            Category category = context.Categories.Find(categoryId);
+            try
+            {
+                if (category != null)
+                {
+                    category.CategoryName = newCategoryName;
+                    context.SaveChanges();
+                    status = true;
+                }
+                else
+                {
+                    status = false;
+                }
+            }
+            catch (Exception)
+            {
+                status = false;
+                throw;
+            }
+            return status;
+        }
+
+        public int UpdateProduct(string productId, decimal price)
+        {
+            int status = -1;
+            try
+            {
+                Product product = context.Products.Find(productId);
+                product.Price = price;
+
+                using (var newContext = new QuickKartDbContext())
+                {
+                    newContext.Products.Update(product);
+                    newContext.SaveChanges();
+                    status = 1;
+                }
+
+            }
+            catch (Exception)
+            {
+                status = -99;
+                throw;
+            }
+            return status;
+        }
+
+        public int UpdateProductsUsingUpdateRange(byte categoryId, int quantityProcured)
+        {
+            int status = -1;
+
+            try
+            {
+                List<Product> productList = context.Products.Where(p => p.CategoryId == categoryId).ToList();
+                foreach (var product in productList)
+                {
+                    product.QuantityAvailable += quantityProcured;
+                }
+                using (var newContext = new QuickKartDbContext())
+                {
+                    newContext.Products.UpdateRange(productList);
+                    newContext.SaveChanges();
+                    status = 1;
+                }
+            }
+            catch (Exception)
+            {
+                status = -99;
+                throw;
+            }
+            return status;
+        }
+
+        public bool UpdateUserPassword(string EmailId, string newUserPassword)
+        {
+            bool status = false;
+            try
+            {
+                User user = context.Users.Where(u => u.EmailId == EmailId).FirstOrDefault();
+                if (user != null)
+                {
+                    user.UserPassword = newUserPassword;
+                    context.SaveChanges();
+                    status = true;
+                }
+                else
+                {
+                    status = false;
+                }
+            }
+            catch (Exception)
+            {
+                status = false;
+                throw;
+            }
+            return status;
+        }
+        #endregion
+
+        #region DELETE
+        public bool DeleteProduct(string productId)
+        {
+            Product product = new Product();
+            bool status = false;
+            try
+            {
+                product = context.Products.Find(productId);
+                if (product != null)
+                {
+                    context.Products.Remove(product);
+                    context.SaveChanges();
+                    status = true;
+                }
+                else
+                {
+                    status = false;
+                }
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public bool DeleteProductsUsingRemoveRange(string subString)
+        {
+            bool status = false;
+            try
+            {
+                var deleteProducts = context.Products.Where(p => p.ProductName.Contains(subString));
+                context.Products.RemoveRange(deleteProducts);
+                context.SaveChanges();
+                status = true;
+            }
+            catch (Exception ex)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public bool DeleteUserDetails(string emailID)
+        {
+            bool status = false;
+            //context.Users.Remove(context.Users.Find(emailID));
+            User user = context.Users.Where(u => u.EmailId == emailID).FirstOrDefault();
+            try
+            {
+                if (user != null)
+                {
+                    context.Users.Remove(user);
+                    context.SaveChanges();
+                    status = true;
+                }
+            }
+            catch (Exception)
+            {
+                status = false;
+                throw;
+            }
+            return status;
+        } 
+        #endregion
+
+
+    }
+}
